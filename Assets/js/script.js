982d674d61ad3df9ba77f15bdd43e171c15c97d5
Ryan <ryanpaynt@uncc.edu>
2021-05-19 15:14:53 -0400

    moving files, improving css, and creating new html page

---
+++ Assets/js/script.js
@@ -0,0 +1,25 @@
+/* 
+vars
+
+declare highscore
+isDone
+timer
+timerCount
+
+var questions = []
+
+function getHighscore
+
+function winGame
+
+function startTimer
+
+function setWins
+
+checkScore
+
+event.target
+
+.matches
+*/
+
