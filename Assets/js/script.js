a33efd5870a08c97b63cad57090e82aee429d3d7
Ryan <ryanpaynt@uncc.edu>
2021-05-19 16:37:08 -0400

    declaring global var and adding an answer to html page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,3 +23,10 @@ event.target
 .matches
 */
 
+var questionEl = document.querySelector('h1');
+var answer1 = document.querySelector('butt1');
+var answer2 = document.querySelector('butt2');
+var answer3 = document.querySelector('butt3');
+var answer4 = document.querySelector('butt4');
+var checkAns = document.querySelector('p');
+
